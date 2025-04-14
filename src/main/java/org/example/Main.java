f2d816b8780b288f57e2291c33fe4f0d02a8f1f3
Minu <testminu1111@gmail.com>
2025-04-15 04:33:04 +0530

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,52 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static ThreadLocal<WebDriver> webDriver = new ThreadLocal<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+        Map<String, Object> prefs = new HashMap<>();
+        // Set preferences for Chrome (e.g., allow camera access)
+        prefs.put("profile.default_content_setting_values.media_stream_camera", 1);  // Allow camera
+        prefs.put("profile.default_content_setting_values.notifications", 2);  // Disable notifications
+
+        try {
+            ChromeOptions options = new ChromeOptions();
+            options.setExperimentalOption("prefs", prefs);
+            options.addArguments("--disable-notifications");
+            options.addArguments("--ignore-ssl-errors=yes");
+            options.addArguments("--ignore-certificate-errors");
+
+            WebDriver driver = new ChromeDriver(options);  // Initialize WebDriver
+            driver.manage().window().maximize();
+
+            driver.get("https://www.google.com");
+            System.out.println("Chrome started successfully!");
+
+            // Wait for a few seconds (in this case, 5 seconds)
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            // Close the browser after the task
+            driver.quit();
+
+
+            // Close browser
+          //  driver.quit();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
         }
-    }
-}
+}}
