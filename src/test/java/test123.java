c8b379767ef8dd00248dad7b2b40f6ac9f3d734e
Minu <testminu1111@gmail.com>
2025-04-15 01:58:22 +0530

    1

---
+++ src/test/java/test123.java
@@ -0,0 +1,22 @@
+public class test123 {
+
+
+    public void test1Tc_001() throws InterruptedException {
+System.out.println("Test1");
+
+    }
+    public void test1Tc_002() throws InterruptedException {
+        System.out.println("Test2");
+
+    }
+    public void test1Tc_003() throws InterruptedException {
+        System.out.println("Test3");
+
+    }  public void test1Tc_004() throws InterruptedException {
+        System.out.println("Test4");
+
+    }  public void test1Tc_005() throws InterruptedException {
+        System.out.println("Test5");
+
+    }
+}
